5cb36a30d489b5e57adf0d59a44bbfa05ad0ae16
Nurdan <turdukulovnurdan90@Gmail.com>
2025-03-02 19:20:47 +0600

    first commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dex2xk&%s+dbb(0m!pcup0iu=s((r35=s$bx-*!fv55jbe!tml'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
